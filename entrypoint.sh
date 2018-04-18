eb2e5c0f636d51bbd13c7e28fd98e5cfeda23cfd
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-04-18 12:11:04 +0200

    fix sed in config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 cp /var/www/platformmanager/Config/conf.ini.sample /var/www/platformmanager/Config/conf.ini
-sed -i 's/MYSQL_URL/${MYSQL_URL}/g' /var/www/platformmanager/Config/conf.ini
-sed -i 's/MYSQL_URL/${MYSQL_DBNAME}/g' /var/www/platformmanager/Config/conf.ini
-sed -i 's/MYSQL_USER/${MYSQL_USER}/g' /var/www/platformmanager/Config/conf.ini
-sed -i 's/MYSQL_PASS/${MYSQL_PASS}/g' /var/www/platformmanager/Config/conf.ini
+sed -i "s/MYSQL_URL/${MYSQL_URL}/g" /var/www/platformmanager/Config/conf.ini
+sed -i "s/MYSQL_URL/${MYSQL_DBNAME}/g" /var/www/platformmanager/Config/conf.ini
+sed -i "s/MYSQL_USER/${MYSQL_USER}/g" /var/www/platformmanager/Config/conf.ini
+sed -i "s/MYSQL_PASS/${MYSQL_PASS}/g" /var/www/platformmanager/Config/conf.ini
 
 exec apache2-foreground
 
