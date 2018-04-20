9ba4a84cf7e82757fabbf4dc112d38bdcbd57a99
Anthony Bretaudeau <anthony.bretaudeau@inria.fr>
2018-04-20 11:56:21 +0200

    add missing vars for apache

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,16 @@ sed -i "s/MYSQL_PASS/${MYSQL_PASS}/g" /var/www/platformmanager/Config/conf.ini
 unset MYSQL_USER
 unset MYSQL_PASS
 
+: "${APACHE_CONFDIR:=/etc/apache2}"
+: "${APACHE_ENVVARS:=$APACHE_CONFDIR/envvars}"
+if test -f "$APACHE_ENVVARS"; then
+    . "$APACHE_ENVVARS"
+fi
+
+# Apache gets grumpy about PID files pre-existing
+: "${APACHE_PID_FILE:=${APACHE_RUN_DIR:=/var/run/apache2}/apache2.pid}"
+rm -f "$APACHE_PID_FILE"
+
 exec apache2 &
 
 sleep 2
